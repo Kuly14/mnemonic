c50c07ced1b0bf87366609c3ec6972470672a3b7
David Kulman <kulman.david@gmail.com>
2022-10-17 23:12:54 +0200

    Add Integration Tests

---
+++ tests/integration_test.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[test]
+pub fn test_workflow() {
+    let mut file = File::create("test.mn").unwrap();
+    file.write_all(b"
+        0x01 // storage slot
+        0x00 mstore // store the storage slot
+        0x4675C7e5BaAFBFFbca748158bEcBA61ef3b0a263 // random address
+        0x20 mstore // store the address at 0x20
+        0x40 0x00
+        sha3 // hash them together to get storage slot for the mapping
+        0xb5d85CBf7cB3EE0D56b3bB207D5Fc4B82f43F511 //another random address
+        sstore
+        ").unwrap();
+
+    // For some reason cargo r -- test.mn > test_bytecode.txt doesn't work
+    let bytecode = Command::new("cargo").args(["r", "--", "test.mn"]).output().unwrap();
+
+    let manual_bytecode = String::from("6001600052734675C7e5BaAFBFFbca748158bEcBA61ef3b0a263602052604060002073b5d85CBf7cB3EE0D56b3bB207D5Fc4B82f43F51155").to_lowercase();
+
+    assert_eq!(String::from_utf8(bytecode.stdout).unwrap().trim(), manual_bytecode);
+}
